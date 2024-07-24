3ed8daf59a2b46016b9c3986211058b528f78c5f
Bilal Mehmoood <bmehmood199@gmail.com>
2024-07-24 17:14:01 +0500

    Refactor code for injecting script into Fiverr pages

--- background.js
+++ background.js
@@ -1,9 +1,18 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        if (/^https:\/\/www\.fiverr/.test(current_tab_info.url)) {
-            chrome.browserAction.onClicked.addListener(() => {
-                chrome.tabs.executeScript(null, { file: 'ui.js' }, () => console.log('inject'))
-            })
-        }
-    })
-})
+// Listen for clicks on the extension's icon.
+chrome.action.onClicked.addListener((tab) => {
+  // Check if the current tab's URL matches the desired pattern.
+  if (/^https:\/\/www\.fiverr/.test(tab.url)) {
+    // Execute the script only if the URL matches.
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id },
+        files: ["ui.js"],
+      })
+      .then(() => {
+        console.log("Script injected");
+      })
+      .catch((error) => {
+        console.error("Failed to inject script: ", error);
+      });
+  }
+});
--- ui.js
+++ ui.js
@@ -1,28 +1,23 @@
+var title = document.querySelectorAll(
+  'p[role="heading"][aria-level="3"][class="QTdEgIS tbody-5 text-normal"][title]'
+);
 
-
-var title = document.querySelectorAll(".gig-card-layout div .gig-wrapper .text-display-7");
-var rating = document.querySelectorAll(".gig-rating span");
-var count_rating = rating.length
-var count_title = title.length
-var count_value = 0;
-
-//This Download function 
+//This Download function
 function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 }
 var all_titles = [];
 
-for (let i = 0; i < count_rating; i++) {
-    if (rating[i].innerText == "(1k+)") {
-        all_titles.push("\n" + title[i].innerText)
-        count_value++
-    }
-
+for (let i = 0; i < title.length; i++) {
+  all_titles.push("\n" + title[i].innerText);
 }
 download("Gig_Title.txt", all_titles);
